98cb3da4ccd0472c9504d341c6ec383ea26607e3
hchinhtrung <chinhtrung1998@gmail.com>
2020-12-12 12:52:57 +0700

    a

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="shadow-md sticky top-0 bg-black text-yellow-400">
+        <div class="container mx-auto px-4 lg:w-2/3 py-2">
+            <div class="flex justify-between">
+                <div class="flex items-center">
+                    <a class="block bg-yellow-400 hover:bg-blue-500 p-0.5 rounded-full" href="/">
+                        <img src="IMG_0131.JPEG" class="w-8 h-8 rounded-full" loading="lazy" alt="">
+                    </a>
+                    <a class="uppercase font-bold text-2xl ml-1" href="/">chinh trung</a>
+                </div>
+                <div class="flex items-center text-lg">
+                    <a class="" href="resume.html">resume</a>
+                    <a class="ml-6" href="devc.html">devc</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
