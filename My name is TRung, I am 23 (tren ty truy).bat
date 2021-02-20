c7c6d49a5952e7501b650f0e71bb42c46811fbbe
chinhtrung <chinhtrung1998@gmail.com>
2021-02-20 12:35:42 +0700

    a

---
+++ My name is TRung, I am 23 (tren ty truy).bat
@@ -0,0 +1,43 @@
+    My name is TRung, I am 23 (tren ty truy) years old. I have graduated from university of Science with the major in computer science. 
+    
+    I have been a Business Analyst executive at Antsomi. (Antsomi is a tech startup focusing on artificial intelligence-driven marketing technology. With the mission to help businesses to transform into data-driven companies, Antsomi built the first AI-enabled customer data platform, CDP 365, in Southeast Asia.)
+    
+    My main task is to 
+    
+    Responsible for closely working with clients/lead team to clarify, analyze the business requirement.
+Get requirement / Research concept, business objective to provide an effective solution for the custome r.
+Use BPMN to model the defined features, use cases, system workflow, and other necessary diagrams.
+Agile/SCRUM, Data-driven knowledge, design the application mock-up/wireframe.
+In charge of document writing of User Requirement Specification, Functional Specification.
+Define the quality attributes, external interfaces, constraints, and other non-functional requirements.
+
+
+    Before that, I worked at an e-commerce company - chotot
+
+
+ 
+
+============================
+
+
+
+    “I believe my best trait is my attention to detail. This trait has helped me tremendously in this field of work.”
+
+“Tôi tin rằng điểm mạnh nhất của tôi là sự quan tâm của tôi đến từng chi tiết. Đặc trưng này đã giúp tôi rất nhiều trong lĩnh vực này.”
+
+    “I’ve always been a great team player. I’m good at keeping a team together and producing quality work in a team environment.”
+
+“Tôi luôn là một đồng đội tuyệt vời. Tôi giỏi duy trì cho nhóm làm việc với nhau và đạt được chất lượng công việc trong một môi trường đồng đội. ”
+
+    “After having worked for a couple of years, I realized my strength is accomplishing a large amount of work within a short period of time. I get things done on time and my manager always appreciated it.”
+
+“Sau khi làm việc một vài năm, tôi nhận ra thế mạnh của tôi là thực hiện một khối lượng lớn công việc trong một khoảng thời gian ngắn. Tôi hoàn thành mọi việc đúng thời hạn và quản lý của tôi luôn luôn đánh giá cao điều đó.
+
+
+
+
+    ++
+
+        “I’ve learned the basics of product management during my first two years. I want to take the next step by taking on challenging projects. My short term goal is to grow as a BI.”
+
+        I would like to become a director or higher. This might be a little ambitious, but I know I’m smart, and I’m willing to work hard.
